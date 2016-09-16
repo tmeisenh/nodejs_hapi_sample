4b6c60d41c96c0a28451febd91b81591c09885a4
Travis Meisenheimer <travis@indexoutofbounds.com>
2016-09-15 22:08:19 -0500

    How about we format before we push?

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,19 +4,19 @@ var helloController = require('src/controllers/hello');
 
 module.exports = function() {
 	return [
-		{
-			method: 'GET',
-			path: '/hello',
-			config : {
-				handler: helloController.hello,
-			}
-		},
-		{
-			method: 'POST',
-			path: '/hello',
-			config : {
-				handler: helloController.hello,
-			}
+	{
+		method: 'GET',
+		path: '/hello',
+		config : {
+			handler: helloController.hello,
 		}
+	},
+	{
+		method: 'POST',
+		path: '/hello',
+		config : {
+			handler: helloController.hello,
+		}
+	}
 	];
 }();
